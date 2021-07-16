bbc16ee6e27aae36bef44869377a8aa5f15fd4c3
KIMHWANSEUNG <49117761+KIMHWANSEUNG@users.noreply.github.com>
2021-07-16 11:34:27 +0900

    Add:heapq 문제 풀이

--- Python/heap_ex1.py
+++ Python/heap_ex1.py
@@ -1,4 +1,24 @@
-def solution(scoville, K):
+import heapq
+
+def solution(scoville,K):
+    answer=0
+    heapq.heapify(scoville)
+    while True:
+        first=heapq.heappop(scoville)
+        if first>K:
+            break
+        if len(scoville)==0:
+            return -1
+        second=heapq.heappop(scoville)
+        heapq.heappush(scoville,first+second*2)
+        answer+=1
+        
+    return answer
+
+
+
+
+def solution2(scoville, K):
     answer = 0
     minscovile=min(scoville)
 
