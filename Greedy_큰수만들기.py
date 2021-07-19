43c7328aacb03b6771621dd10b4ba015c1cdaf8b
KIMHWANSEUNG <49117761+KIMHWANSEUNG@users.noreply.github.com>
2021-07-19 20:07:58 +0900

    Add: 그리디 탐욕법

---
+++ Greedy_큰수만들기.py
@@ -0,0 +1,49 @@
+def solution(number, k):
+    stk=[]
+    
+    for i in number:
+        while stk and stk[-1]<i and k>0:
+            k-=1
+            stk.pop()
+        stk.append(i)
+        print(len(stk))
+    return "".join(stk[:len(stk)-k])
+
+
+#"94"
+print(solution("1924",2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def solution2(number, k):
+    stack = [number[0]]
+    for num in number[1:]:
+        while len(stack) > 0 and stack[-1] < num and k > 0:
+            k -= 1
+            stack.pop()
+        stack.append(num)
+    if k != 0:
+        stack = stack[:-k]
+    return ''.join(stack)
