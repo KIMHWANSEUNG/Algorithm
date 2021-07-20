88354d99ba71488638822a6d36d7ec81fd324dd5
KIMHWANSEUNG <49117761+KIMHWANSEUNG@users.noreply.github.com>
2021-07-20 17:10:05 +0900

    add: 해시 위장(문제)

--- hash_위장.py
+++ hash_위장.py
@@ -1,3 +1,10 @@
+#https://programmers.co.kr/learn/courses/30/lessons/42578
+
 def solution(clothes):
     answer = 0
+    clothes=map(clothes)
+    print(clothes)
+    
     return answer
+
+print(solution([["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]))
