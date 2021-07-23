d324ac6b7ade6436b5e7c76a1797993597a1ffea
KIMHWANSEUNG <49117761+KIMHWANSEUNG@users.noreply.github.com>
2021-07-23 13:42:43 +0900

    add: 풀이완료

--- 카카오블라인드채용_1차비밀지도.py
+++ 카카오블라인드채용_1차비밀지도.py
@@ -1,11 +1,14 @@
 def solution(n, arr1, arr2):
     answer = []
 
-    
     for a1,a2 in zip(arr1,arr2):
         arr12=str(bin(a1|a2))[2:]
-        print(arr12)
-        arr12[1].count(0)
+        arr12 = arr12.rjust(n,'0')
+        arr12=arr12.replace('1','#')
+        arr12=arr12.replace('0',' ')
+        answer.append(arr12)
+    print(answer)        
     return answer
 
 solution(5,[9, 20, 28, 18, 11],	[30, 1, 21, 17, 28])
+solution(6,[46, 33, 33 ,22, 31, 50],[27 ,56, 19, 14, 14, 10])
