a25754c05022c04ffc114019b2972b5b54269ab7
KIMHWANSEUNG <ghkstmd100@naver.com>
2022-02-09 01:06:44 +0900

    풀이완료

--- Python/메뉴리뉴얼.py
+++ Python/메뉴리뉴얼.py
@@ -3,18 +3,28 @@ import itertools
 
 def solution(orders, course):
     answer = []
-
+    ## 가장 많이 시킨 코스요리 찾기
     for size_of_course in course:
-        orders_dict = {}
-        orders_combination = []
+        order_dict= {}
+        order_combinations = []
         for order in orders:
-            orders_combination.extend(list(itertools.combinations(sorted(order),size_of_course)))
-        print(orders_combination)
+            print(list(itertools.combinations(sorted(order),size_of_course)))
+            order_combinations.extend(list(itertools.combinations(sorted(order),size_of_course)))
 
-            
+        for order_combination in order_combinations:
+            print(order_combination)
+            order_str = ''.join(order_combination)
+            if order_str in order_dict :
+                order_dict[order_str]+=1
+            else:
+                order_dict[order_str] = 1
+        print(order_dict)  
 
-    
-        
-    return answer
+        for k,v in order_dict.items():
+            if order_dict[k] == max([order_dict[order] for order in order_dict]):
+                if order_dict[k] > 1:
+                    answer.append(k)
+
+    return sorted(answer)
 
 print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"],[2,3,4]))
