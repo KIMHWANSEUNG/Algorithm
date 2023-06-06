cd71b28e60487fb7687712f67a145961f8441b4b
KIMHWANSEUNG <ghkstmd100@naver.com>
2023-06-06 14:37:05 +0900

    레벨1 문제풀이

---
+++ Java/Programmers/Lv1/BackgroudClean.java
@@ -0,0 +1,51 @@
+package Programmers.Lv1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+public class BackgroudClean {
+//["..........", ".....#....", "......##..", "...##.....", "....#....."]
+    static String[] wallpaper = {"..........", ".....#....", "......##..", "...##.....", "....#....."};
+
+    public static int[] solution(String[] wallpaper){
+
+
+        ArrayList<Integer> xArr = new ArrayList<>();
+        ArrayList<Integer> yArr = new ArrayList<>();
+
+        int yIndex = 0;
+        for(String wall : wallpaper){
+            //제일 최소 x축과 y축 구하기
+            //제일 최대 x축 y축 구하기
+            int xIndex = 0;
+            char[] wallArr = wall.toCharArray();
+            for(char wallChar : wallArr){
+                if(wallChar == '#'){
+                    xArr.add(xIndex);
+                    yArr.add(yIndex);
+                    System.out.println(yIndex+"------"+xIndex);
+                }
+                xIndex++;
+            }
+            yIndex++;
+        }
+
+        int xMin = Collections.min(xArr);
+        int xMax = Collections.max(xArr)+1;
+        int yMin = Collections.min(yArr);
+        int yMax = Collections.max(yArr)+1;
+
+
+
+        int []result = {yMin,xMin,yMax,xMax};
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(wallpaper));
+
+
+    }
+}
--- Java/Programmers/Lv1/ParkWalking.java
+++ Java/Programmers/Lv1/ParkWalking.java
@@ -1,5 +1,8 @@
 package Programmers.Lv1;
 
+import org.jcp.xml.dsig.internal.dom.DOMTransform;
+import sun.misc.InnocuousThread;
+
 import java.lang.reflect.Type;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -8,54 +11,68 @@ import java.util.Map;
 
 public class ParkWalking {
 
-    static String[] park ={ "SOO",
-            "OOO",
-            "OOO"};
-    static String[] routes = {"E 2","S 2","W 1"};
-       //result = [2,1]
-      public static int[] solution(String[] park, String[] routes) {
+    static String[] park = {"SOO",
+        "OOO",
+        "OOO"};
+    static String[] routes = {"E 2", "S 2", "W 1"};
 
-          Map<String,Integer> parkMap = new HashMap<>();
-          Map<String,Integer> routesMap = new HashMap<>();
+    //result = [2,1]
+    public static int[] solution(String[] park, String[] routes) {
 
-          //find 'S' index
-          int xIndex = 0;
-          int yIndex = 0;
-          for(String parkString : park){
-            List<String> parkArr = Arrays.asList(parkString.split(""));
-            if(parkArr.contains("S")){
-                xIndex = parkArr.indexOf("S");
-                yIndex = parkMap.size();
+        char[][] arr = new char[park.length][park[0].length()];
+        //find 'S' index
+        int xIndex = 0;
+        int yIndex = 0;
+        for(int i =0;i< park.length;i++){
+            for(int j=0;j<park[0].length();j++){
+                arr[i][j] = park[i].charAt(j);
+                if(arr[i][j]=='S'){
+                    xIndex = j;
+                    yIndex = i;
+                }
             }
-          }
-          int[] result = {xIndex,yIndex};
+        }
 
 
-          for(String route : routes){
-            String[] routeArr = route.split(" ");
-            if(routeArr[0].equals("S") || routeArr[0].equals("W")){
-                routesMap.put(routeArr[0],-(Integer.parseInt(routeArr[1])));
-            }else {
-                routesMap.put(routeArr[0],Integer.parseInt(routeArr[1]));
+        for (String route : routes) {
+            String way = route.split(" ")[0];
+            int len = Integer.parseInt(route.split(" ")[1]);
+
+            int nx = xIndex;
+            int ny = yIndex;
+
+            for (int i = 0; i < len; i++) {
+                if (way.equals("E")) {
+                    nx++;
+                }
+                if (way.equals("W")) {
+                    nx--;
+                }
+                if (way.equals("S")) {
+                    ny++;
+                }
+                if (way.equals("N")) {
+                    ny--;
+                }
+                if (nx >= 0 && ny >= 0 && nx < arr[0].length && ny < arr.length) {
+                    if (arr[ny][nx] == 'X') {
+                        break;
+                    }
+                    //장애물 x일 경우
+                    if (i == len - 1) {
+                        xIndex = nx;
+                        yIndex = ny;
+                    }
+                }
             }
-          }
+        }
 
-          routesMap.forEach((key,value) -> {
-              if(key.equals("W") || key.equals("E")){
-                  result[1] += value;
-              }else {
-                  result[0] += value;
-              }
-          });
 
-          for(int i=0; i<result.length; i++){
-              result[i]= Math.abs(result[i]);
 
-          }
-          System.out.println(Arrays.toString(result));
+        int[] result = {yIndex, xIndex};
+        return result;
+    }
 
-          return result;
-      }
     public static void main(String[] args) {
         solution(park, routes);
 
